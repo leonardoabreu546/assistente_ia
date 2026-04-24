3985ccc329d65a75d9a733a08821126ed2b84291
Leonardo <lcat140507@gmail.com>
2026-04-24 08:54:22 +0100

    remover código desnecessário

--- assistente_ia/src/App.tsx
+++ assistente_ia/src/App.tsx
@@ -1,11 +1,9 @@
-import { useState } from 'react'
-import Home from './pages/Home.tsx'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
   return (
-    <Home description="Assistente de IA" />    
+    <Home description="Assistente de IA" />
   )
 }
 
