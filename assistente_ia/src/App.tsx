4b793744f2b2e21d8326fea56c45a4eb053aa5d7
Leonardo <lcat140507@gmail.com>
2026-04-26 22:05:08 +0100

    implementar react router

--- assistente_ia/src/App.tsx
+++ assistente_ia/src/App.tsx
@@ -1,13 +1,20 @@
 import Home from './pages/Home'
 import About from './pages/About'
+import Footer from './components/footer'
+import Header from './components/header'
+import { Routes, Route } from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
 function App() {
   return (
     <>
-      <Home description="Assistente de IA" />
-      <About description="Este é um assistente de inteligência artificial desenvolvido para ajudar os usuários a obter informações e realizar tarefas de forma eficiente." />
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home description="Assistente de IA"/>} />
+        <Route path="/about" element={<About description="Este é um assistente de inteligência artificial desenvolvido para ajudar os usuários a obter informações e realizar tarefas de forma eficiente."/>} />
+      </Routes>
+      <Footer/>
     </>
   )
 }
--- assistente_ia/src/components/header.tsx
+++ assistente_ia/src/components/header.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <a className="navbar-brand" href="/">Assistente IA</a>
+        <Link className="navbar-brand" to="/">Assistente IA</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item">
-              <a className="nav-link" href="/">Home</a>
+              <Link className="nav-link" to="/">Home</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/chat">Chat</a>
+              <Link className="nav-link" to="/chat">Chat</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/history">Histórico</a>
+              <Link className="nav-link" to="/history">Histórico</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="/dashboard">Dashboard</a>
+              <Link className="nav-link" to="/dashboard">Dashboard</Link>
             </li>
           </ul>
         </div>
