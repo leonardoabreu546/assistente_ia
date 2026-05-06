da8aaab8ed7ea5cb0fb2ec35eead45f1422c8263
Leonardo <lcat140507@gmail.com>
2026-05-06 18:33:24 +0100

    adicionar componentes

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import { auth } from "../firebase/firebaseConfig"
 import { signInWithEmailAndPassword } from "firebase/auth"
-import { useNavigate, Link } from "react-router-dom"
+import { useNavigate } from "react-router-dom"
+import AuthCard from "../components/AuthCard"
+import LoginForm from "../components/LoginForm"
 
-function Login() {
+export default function Login() {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const navigate = useNavigate()
@@ -19,45 +21,14 @@ function Login() {
   }
 
   return (
-    <div className="container mt-5">
-      <div className="row justify-content-center">
-        <div className="col-12 col-md-10 col-lg-8"> 
-          <div className="shadow-sm p-4 p-md-5 bg-white rounded">
-            <h2 className="mb-4 fw-bold">Login</h2>
-
-            <form onSubmit={handleLogin}>
-              <div className="mb-3">
-                <input
-                  className="form-control py-2" // Removido o -sm e adicionado py-2 para mais altura vertical
-                  placeholder="Email"
-                  value={email}
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-
-              <div className="mb-4">
-                <input
-                  type="password"
-                  className="form-control py-2" // Removido o -sm e adicionado py-2 para mais altura vertical
-                  placeholder="Password"
-                  value={password}
-                  onChange={(e) => setPassword(e.target.value)}
-                />
-              </div>
-
-              <button className="btn btn-primary w-100 py-2">
-                Entrar
-              </button>
-            </form>
-
-            <p className="mt-4 text-center text-muted">
-              Não tens conta? <Link to="/register" className="text-decoration-none">Registar</Link>
-            </p>
-          </div>
-        </div>
-      </div>
-    </div>
+    <AuthCard title="Login">
+      <LoginForm 
+        email={email}
+        setEmail={setEmail}
+        password={password}
+        setPassword={setPassword}
+        onSubmit={handleLogin}
+      />
+    </AuthCard>
   )
 }
-
-export default Login
