e51a318d3cc2c7189a0ac78fc0640d4b0b856011
Leonardo <lcat140507@gmail.com>
2026-05-06 17:12:37 +0100

    alterar componente tinybarchart

--- src/components/TinyBarChart.tsx
+++ src/components/TinyBarChart.tsx
@@ -1,28 +1,29 @@
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip } from 'recharts'
 
-const TinyBarChart = ({ data }) => {
+interface ChartData {
+  name: string;
+  messages: number;
+}
+
+const TinyBarChart = ({ data }: { data: ChartData[] }) => {
   const chartData = data && data.length > 0 ? data : [
     { name: 'Sem dados', messages: 0 }
   ]
 
   return (
-    <div style={{ display: "flex", justifyContent: "center" }}>
+    // Removendo o ResponsiveContainer e usando largura fixa para teste
+    <div className="d-flex justify-content-center w-100">
       <BarChart
-        width={300}
-        height={150}
+        width={500} // Largura fixa para garantir que renderize
+        height={250}
         data={chartData}
-        margin={{
-          top: 5,
-          right: 30,
-          left: 20,
-          bottom: 5,
-        }}
+        margin={{ top: 5, right: 30, left: 0, bottom: 5 }}
       >
-        <CartesianGrid strokeDasharray="3 3" />
+        <CartesianGrid strokeDasharray="3 3" vertical={false} />
         <XAxis dataKey="name" />
         <YAxis />
         <Tooltip />
-        <Bar dataKey="messages" fill="#8884d8" />
+        <Bar dataKey="messages" fill="#0d6efd" radius={[4, 4, 0, 0]} />
       </BarChart>
     </div>
   );
