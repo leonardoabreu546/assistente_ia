c2ab8ecfe86ccc3284a3a9259eb0857df11c1281
Leonardo <lcat140507@gmail.com>
2026-05-06 18:18:17 +0100

    adicionar componentes a aboutdetails

---
+++ src/components/AboutDetails.tsx
@@ -0,0 +1,12 @@
+// src/components/AboutDetails.tsx
+export default function AboutDetails() {
+  return (
+    <div className="row justify-content-center mt-5">
+      <div className="col-md-8 text-center">
+        <p className="text-muted small">
+          Este assistente foi criado para demonstrar o poder da IA combinada com uma interface moderna.
+        </p>
+      </div>
+    </div>
+  );
+}
