adbf93e7a3994d406f02189980d0afe2dbfdc707
Leonardo <lcat140507@gmail.com>
2026-05-06 19:32:46 +0100

    adicionar link ao header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import { auth } from '../../firebase/firebaseConfig'
-import { signOut } from 'firebase/auth';
-import type { User } from 'firebase/auth';
+import { signOut } from 'firebase/auth'
+import type { User } from 'firebase/auth'
 import { useTheme } from '../../hooks/useTheme'
 
 function Header() {
-
   const [user, setUser] = useState<User | null>(null)
   const { theme, toggleTheme } = useTheme()
 
@@ -33,9 +32,12 @@ function Header() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
+          <ul className="navbar-nav ms-auto align-items-center">
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">Sobre</Link>
             </li>
             <li className="nav-item">
               <Link className="nav-link" to="/chat">Chat</Link>
@@ -46,19 +48,30 @@ function Header() {
             <li className="nav-item">
               <Link className="nav-link" to="/dashboard">Dashboard</Link>
             </li>
-            {user && (
-              <li className="nav-item me-2">
+
+            {!user ? (
+              <>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/login">Login</Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to="/register">Registar</Link>
+                </li>
+              </>
+            ) : (
+              <li className="nav-item ms-lg-2">
                 <button className="btn btn-outline-light btn-sm" onClick={handleLogout}>
                   Logout
                 </button>
               </li>
             )}
-            <li className="nav-item">
+
+            <li className="nav-item ms-lg-3">
               <button 
                 className={`btn btn-sm ${theme === 'dark' ? 'btn-warning' : 'btn-primary'}`}
                 onClick={toggleTheme}
               >
-                {theme === 'dark' ? '☀️ Light' : '🌙 Dark'}
+                {theme === 'dark' ? '☀️' : '🌙'}
               </button>
             </li>
           </ul>
